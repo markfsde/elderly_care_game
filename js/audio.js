a537c85583ba37a8dee001961ae4c9d46ff39b2b
zyy <yuyue.23@intl.zju.edu.cn>
2026-06-09 14:25:40 +0800

    背景音乐间隔

--- js/audio.js
+++ js/audio.js
@@ -72,7 +72,7 @@ const GameAudio = (() => {
     }
 
     // 下一段
-    bgmScheduler = setTimeout(scheduleBGM, (barLen + 1) * 1000);
+    bgmScheduler = setTimeout(scheduleBGM, (barLen + 0.2) * 1000);
   }
 
   function startBGM() {
